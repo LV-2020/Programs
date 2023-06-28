8048b1cc4ee162f1a7c2c253b04a1c7031feedc9
vlanka <vasanthi.lanka@gmail.com>
2023-06-28 13:17:33 +0530

    changes main1.py

--- main1.py
+++ main1.py
@@ -1,1 +1,9 @@
+### Practice Python programs
+### To print the reverse of a string in python
+import pandas as pd
+import numpy as np
+
+str = input("Enter a gien string")
+rev = str[::-1]
+print(rev)
 
